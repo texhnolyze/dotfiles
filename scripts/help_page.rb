85731491a41a8248982a0385c04e0af8b0b2faa3
texhnolyze <joern-g@web.de>
2014-09-15 17:22:16 +0200

    improved ruby google spreadsheet conversion script to take a destination folder arg

--- scripts/help_page.rb
+++ scripts/help_page.rb
@@ -1,10 +1,10 @@
 #!/bin/ruby
 
 require 'open-uri'
-require 'pp'
 require 'csv'
 
 $url = 'https://docs.google.com/spreadsheets/d/1ZaT_hW_yDMdzrRDaOoGDomF4FgJpwuViC4rUERg2Qno/export?format=csv&id=1ZaT_hW_yDMdzrRDaOoGDomF4FgJpwuViC4rUERg2Qno&gid=0'
+$destination_folder = ARGV.first
 
 def create_files(folder = './')
 	file = File.new("#{folder}lang.de", 'w+')
@@ -14,12 +14,12 @@ def create_files(folder = './')
 	file = File.new("#{folder}lang.en", 'w+')
 end
 
-def csv_from_url(url = $url)
-	csv = CSV.new(open(url), :headers => :first_row)
+def get_csv_from_url(url = $url)
+	CSV.new(open(url), :headers => :first_row)
 end
 
-def iterate
-	csv_from_url.each do |row|
+def split_csv_into_files(csv)
+	csv.each do |row|
 	 	if portal?(row)
 			business_units(row)
 		end
@@ -39,37 +39,40 @@ def business_units(row)
 end
 
 def srf(row)
-	write_tranlation_file('./lang.de', 'de (SRF)', row)
+	write_tranlation_file('lang.de', 'de (SRF)', row)
 end
 
 def rts(row)
-	write_tranlation_file('./lang.fr', 'fr (RTS)', row)
+	write_tranlation_file('lang.fr', 'fr (RTS)', row)
 end
 
 def rsi(row)
-	write_tranlation_file('./lang.it', 'it (RSI)', row)
+	write_tranlation_file('lang.it', 'it (RSI)', row)
 end
 
 def rtr(row)
-	write_tranlation_file('./lang.gsw', 'rm (RTR)', row)
+	write_tranlation_file('lang.gsw', 'rm (RTR)', row)
 end
 
 def swi(row)
-	write_tranlation_file('./lang.en', 'en (SWI)', row)
+	write_tranlation_file('lang.en', 'en (SWI)', row)
 end
 
-
 def write_tranlation_file(file, bu_key, row)
-	File.open(file, 'a+') do |f| 
+	File.open("#{$destination_folder}" + file, 'a+') do |f| 
 		key = row["Key Web"].to_s
-		translation = escape_quotes(row[bu_key].to_s)
+		translation = strip_newlines(row[bu_key].to_s)
 		f.write(%{#{key} = "#{translation}" \n}) 
 	end
 end
 
-def escape_quotes(string)
-	string.gsub('"', '\"').gsub(/\n/, '')
+def strip_newlines(string)
+	string.gsub(/\n/, '');
 end
 
-create_files
-iterate
+def run
+	create_files($destination_folder)
+	split_csv_into_files(get_csv_from_url)
+end
+
+run
