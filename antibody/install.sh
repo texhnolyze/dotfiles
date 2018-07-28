e6b8301e7d092daa073411ce10944c1eb2d30774
texhnolyze <joegriepen@gmail.com>
2018-07-28 13:24:30 +1000

    refac(antibody): do not reinstall antibody

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if type yay > /dev/null 2>&1; then
-	yay -S --needed --noconfirm antibody
+  pacman -Q antibody || yay -S --needed --noconfirm antibody
   antibody bundle < "$HOME/dotfiles/antibody/bundles.txt" > ~/.zsh_plugins.sh
   #antibody update
 else
