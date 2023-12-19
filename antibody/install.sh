8c97c1a4505d0713b1bf36e9600e9998915813fe
texhnolyze <joegriepen@gmail.com>
2023-12-19 12:56:24 +0100

    fix(antibody): setup/update script

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 install_antibody() {
-  if test yay &> /dev/null; then
+  if type yay &> /dev/null; then
     pacman -Q antibody || yay -S --needed --noconfirm antibody
-  elif test dpkg &> /dev/null; then
-    local dpkg_download_url="$(curl -s https://api.github.com/repos/getantibody/antibody/releases/latest | jq '.assets[] | select(.name | contains("amd64.deb")) | .browser_download_url')"
-    curl -sL "$dpkg_download_url" /tmp/antibody.deb
-    dpkg -i /tmp/antibody.deb
+  elif type dpkg &> /dev/null; then
+    local dpkg_download_url="$(curl -fsSL \
+      'https://api.github.com/repos/getantibody/antibody/releases/latest' \
+      | jq '.assets[] | select(.name | contains("amd64.deb")) | .browser_download_url'\
+    )"
+    echo "$dpkg_download_url"
+    curl -fsSL -o /tmp/antibody.deb "$dpkg_download_url"
+    sudo dpkg -i /tmp/antibody.deb
   else
     echo "none of the normal installation methods supported"
     echo "falling back to curl | sh -s"
